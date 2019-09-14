dd7254b9bf4f5aaffb7da1d41baf8e4ec6fe3d22
Niclas 'Fashdey' Andersen <Fashdey@gmail.com>
2019-09-14 15:27:13 +0200

    gitflow-feature-stash: bookanddbsetup
    Book Model added
    Database Connection+Setup Added

---
+++ BookshelfAPI/BookshelfAPI/Models/Books.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+namespace BookshelfAPI.Models
+{
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string ISBN { get; set; }
+
+        public int PublishYear { get; set; }
+
+        public string Description { get; set; }
+
+        public string Author { get; set; }
+
+        public string Image { get; set; }
+
+    }
+}
