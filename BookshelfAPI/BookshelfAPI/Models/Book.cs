a5d144e3c74c4ad52aa6806352caad0beb13034c
Niclas 'Fashdey' Andersen <Fashdey@gmail.com>
2019-09-14 16:30:16 +0200

    gitflow-hotfix-stash: dbSettingsWrongDB
    DB Fix and adjusted Model to represent Actual DB fields ++ Some cleanup due to some configuration at project start

--- BookshelfAPI/BookshelfAPI/Models/Book.cs
+++ BookshelfAPI/BookshelfAPI/Models/Book.cs
@@ -10,16 +10,22 @@ namespace BookshelfAPI.Models
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
 
+        [BsonElement("title")]
         public string Title { get; set; }
 
+        [BsonElement("isbn")]
         public string ISBN { get; set; }
 
+        [BsonElement("publishYear")]
         public int PublishYear { get; set; }
 
+        [BsonElement("description")]
         public string Description { get; set; }
 
+        [BsonElement("author")]
         public string Author { get; set; }
 
+        [BsonElement("image")]
         public string Image { get; set; }
 
     }
